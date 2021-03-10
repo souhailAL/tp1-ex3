8d77e9b6ec34493f25a26e92b482e01aa467d93b
souhail <souhail@souhail-PC>
2021-03-10 14:52:13 +0100

    this is exercice 3

---
+++ src/isetj/servlets/SearchServlet.java
@@ -0,0 +1,55 @@
+package isetj.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;;
+
+/**
+ * Servlet implementation class SearchServlet
+ */
+@WebServlet("/SearchServlet")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String v=request.getParameter("VotreRecherche");
+		String m=request.getParameter("m1");
+		request.setAttribute("VotreRecherche",v);
+		request.setAttribute("m1",m);
+		
+	
+		if(m.equals("Google")){
+			response.sendRedirect("http://www.google.com/search?q="+v);
+			}
+			else{
+			response.sendRedirect("http://search.yahoo.com/search?q="+v);
+			}
+		}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
